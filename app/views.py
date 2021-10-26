12850dec1b6744761d0df26679a9db2bc14885e5
Otto Ronkainen <otto.ronkainen@gmail.com>
2021-10-26 13:25:06 +0300

    EkaPaiva

--- app/views.py
+++ app/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 
+from EkaDjangoProjekti.app.models import Supplier
+from .models import Supplier, Product
+
 def landgingview(request):
     return render(request, "landingpage.html")
 
-# Create your views here.
+def supplierlistview(request):
+    supplierlist = Supplier.objects.all()
+    context = {'suppliers': supplierlist}
+    return render (request, "suppliers.html", context)
+
+def productslistview(request):
+    productslist = Product.objects.all()
+    context = {'product': productslist}
+    return render (request, "products.html", context)
+
