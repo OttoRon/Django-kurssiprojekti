ac9fd5aadf242c4528c2a401a1b2f076c83d2e4c
Otto Ronkainen <otto.ronkainen@gmail.com>
2021-11-02 12:55:15 +0200

    Lisätty formi

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import landgingview,productlistview,supplierlistview
+from .views import landgingview,productlistview,supplierlistview, addsupplier
 from app import views
 
 urlpatterns = [
     path('', landgingview),
     path('suppliers.html/',supplierlistview),
+    path('add-supplier/', addsupplier),
     #path('products/', productlistview),
     path('products.html/', productlistview),
 ]
--- app/views.py
+++ app/views.py
@@ -10,6 +10,16 @@ def supplierlistview(request):
         context = {'suppliers': supplierlist}
         return render (request, "suppliers.html", context)
 
+def addsupplier(request):
+        a = request.POST['companyname']
+        b = request.POST['contactname']
+        c = request.POST['address']
+        d = request.POST['phone']
+        e = request.POST['email']
+        f = request.POST['country']
+        Supplier(companyname = a, contactname = b, address = c, phone = d, email = e, country = f).save()
+        return redirect(request.META['HTTP_REFERER'])
+
 # def productlistview(request):
 #     productslist = Product.objects.all()
 #     context = {'product': productslist}
@@ -33,7 +43,6 @@ def addproduct(request):
         c = request.POST['unitprice']
         d = request.POST['unitsinstock']
         e = request.POST['supplier']
-        
         Product(productname = a, packagesize = b, unitprice = c, unitsinstock = d, supplier = Supplier.objects.get(id = e)).save()
         return redirect(request.META['HTTP_REFERER'])
       
